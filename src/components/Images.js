fae893273581dabe34530a4367dfc83863c8d866
nathaniel-grandelis <grandelisn@gmail.com>
2021-01-22 13:31:10 -0500

    mvp y'all

---
+++ src/components/Images.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const images = [
+    {
+        name: 'Luke Skywalker',
+        imageUrl: 'https://i.redd.it/3dwb6p8otc931.jpg'
+
+    },
+    {
+        name: 'C-3PO',
+        imageUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2df9363a-3023-48fa-8cba-35e28ae2dd85/d9kb64r-99e77607-fc03-4ec1-821b-6a1adf005688.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMmRmOTM2M2EtMzAyMy00OGZhLThjYmEtMzVlMjhhZTJkZDg1XC9kOWtiNjRyLTk5ZTc3NjA3LWZjMDMtNGVjMS04MjFiLTZhMWFkZjAwNTY4OC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.v-ZrsOPqJEfEru4Q-qvKDrQ8VYrZRsh1ju0M83Tp3ZE'
+
+    },
+    {
+        name: 'R2-D2',
+        imageUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/r2d2-aswego-arts.jpg'
+
+    },
+    {
+        name: 'Darth Vader',
+        imageUrl: 'https://i.pinimg.com/originals/5b/31/3d/5b313d28c26be28cfbc5d74da3e0ea19.jpg'
+
+    },
+    {
+        name: 'Leia Organa',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61gXLRFYhFL._SL1020_.jpg'
+
+    },
+    {
+        name: 'Owen Lars',
+        imageUrl: 'https://comicvine1.cbsistatic.com/uploads/original/6/62795/5012547-star%20wars.jpg'
+
+    },
+    {
+        name: 'Beru Whitesun Lars',
+        imageUrl: 'https://theartmad.com/wp-content/uploads/2015/06/Beru-Lars-2.jpg'
+
+    },
+    {
+        name: 'R5-D4',
+        imageUrl: 'https://i.pinimg.com/736x/d0/8b/91/d08b916045b83caebc2e0ae149b59fe1.jpg'
+
+    },
+    {
+        name: 'Biggs Darklighter',
+        imageUrl: 'https://i.pinimg.com/originals/89/34/16/89341659032f0c0540ad38d3cde8f435.jpg'
+
+    },
+    {
+        name: 'Obi-Wan Kenobi',
+        imageUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/obi-wan-kenobi-star-wars-998-jovemini-art.jpg'
+
+    }
+] 
